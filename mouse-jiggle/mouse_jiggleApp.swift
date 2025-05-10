43c12c0ec7d5b2f99962f7c23d79b38a43270dde
Bradicalone <b.vanderbush@yahoo.com>
2025-05-09 17:40:24 -0700

    Initial Commit

---
+++ mouse-jiggle/mouse_jiggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  mouse_jiggleApp.swift
+//  mouse-jiggle
+//
+//  Created by brad vanderbush on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct mouse_jiggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
