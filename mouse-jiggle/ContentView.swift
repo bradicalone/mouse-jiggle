20a68eadef53f47bd060193e70b78398f94a4811
Bradicalone <b.vanderbush@yahoo.com>
2025-05-10 09:45:34 -0700

    init commit

--- mouse-jiggle/ContentView.swift
+++ mouse-jiggle/ContentView.swift
@@ -10,15 +10,24 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+            Image("joystick")
+                .resizable()
+                .cornerRadius(5)
+//                .scaledToFit()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100.0, height: 32.0)
+                
+                
             Text("Hello, world!")
+                .foregroundColor(Color.white)
         }
         .padding()
+        .background(Color.black)
+        
     }
 }
 
 #Preview {
     ContentView()
 }
+
